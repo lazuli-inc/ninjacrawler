e0d5e8c467c680a1d9c001984d0d7abee6c4bcad
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-06-27 10:21:46 +0600

    Update README.md formatting and remove unused environment variables file

--- examples/concurrent/constant/collection.go
+++
@@ -1,8 +0,0 @@
-package constant
-
-const Categories = "categories"
-const SubCategories = "sub_categories"
-const Series = "series"
-const Products = "products"
-const Other = "others"
-const ProductDetails = "product_details"

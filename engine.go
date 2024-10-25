28a943a51458c28202e25586d5f3eb420e4e59d6
Uzzal Hosen <uzzalh.cse@gmail.com>
2024-10-25 15:16:57 +0600

    added JsInstructions flag

--- engine.go
+++ engine.go
@@ -59,6 +59,7 @@ type Engine struct {
 type ProviderQueryOption struct {
 	JsRender             bool
 	UsePremiumProxyRetry bool
+	JsInstructions       bool
 	CustomHeaders        bool
 	PremiumProxy         bool
 	ProxyCountry         string
@@ -143,6 +144,9 @@ func (app *Crawler) BuildQueryString() string {
 
 	if app.engine.ProviderOption.JsRender {
 		params.Add("js_render", "true")
+	}
+	if app.engine.ProviderOption.JsInstructions {
+		params.Add("js_instructions", "js_instructions")
 	}
 	if app.engine.ProviderOption.CustomHeaders {
 		params.Add("custom_headers", "true")
